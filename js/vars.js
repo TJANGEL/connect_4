f6aa7a32d451a5cec0d6b68e43bbbc3d8394bab8
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-05-17 19:35:04 -0400

    added game variables js file

---
+++ js/vars.js
@@ -0,0 +1,27 @@
+// Create variables.
+var Game = {};
+
+// Global game config
+Game.config = {
+  startingPlayer: 'black',
+  takenMsg: 'This position is already taken. Please make another choice.',
+  drawMsg: 'This game is a draw.',
+  winMsg: 'The winner is: ',
+  countToWin: 4,
+
+  // note: board dimensions are zero-indexed
+  boardLength: 6,
+  boardHeight: 5
+};
+
+// Global Game State
+Game.board = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0]
+];
+
+Game.currentPlayer = Game.config.startingPlayer;
